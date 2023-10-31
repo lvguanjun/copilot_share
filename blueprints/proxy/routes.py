4141df35086bbc852ca592ca9dd5cf9513749ad4
lvguanjun <lvguanjun135@foxmail.com>
2023-10-31 23:04:25 +0800

    Use origin head and body request copilot chat
    Signed-off-by: lvguanjun <lvguanjun135@foxmail.com>

--- blueprints/proxy/routes.py
+++ blueprints/proxy/routes.py
@@ -109,19 +109,36 @@ async def proxy_copilot_chat_completion_v1():
     if status_code != 200:
         return jsonify(copilot_token), status_code
     copilot_token = copilot_token.get("token")
-
     headers = {
         "Authorization": f"Bearer {copilot_token}",
-        "Editor-Version": "vscode/1.82.2",
-        "Editor-Plugin-Version": "copilot-chat/0.7.1",
+        "X-Request-Id": "a213f6b8-1c44-4f32-b8f2-738dbcd19a12",
+        "Vscode-Sessionid": "9b7f34a2-45d3-4c00-b123-876f45bc7d8c9847215637890",
+        "Vscode-Machineid": (
+            "e0ef0c6ace8d45b9b6ee8cb47092ceb5e0ef0c6ace8d45b9b6ee8cb47092ceb5"
+        ),
+        "Editor-Version": "vscode/1.83.1",
+        "Editor-Plugin-Version": "copilot-chat/0.8.0",
         "Openai-Organization": "github-copilot",
         "Openai-Intent": "conversation-panel",
         "Content-Type": "application/json",
-        "User-Agent": "GitHubCopilotChat/0.7.1",
+        "User-Agent": "GitHubCopilotChat/0.8.0",
         "Accept": "*/*",
+        "Accept-Encoding": "gzip,deflate,br",
+        "connection": "close",
     }
     json_data = request.get_json()
+    messages = json_data["messages"]
+    model = json_data.get("model", "copilot-chat")
     is_stream = json_data.get("stream", False)
+    # 使用 copilot-chat 原始请求体
+    json_data = {
+        "messages": messages,
+        "model": "gpt-4",
+        "temperature": 0.1,
+        "top_p": 1,
+        "n": 1,
+        "intent": True,
+    }
     new_request = fake_request("POST", json=json_data, headers=headers)
     res = await proxy_request(new_request, CHAT_COMPLETION_URL)
     if is_stream:
